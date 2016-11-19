3ef2599a1ca97b4343c30590153dd6c8e8a63097
Felipe Tovar <f.tovar12@hotmail.com>
2016-11-19 17:38:13 -0500

    first commit

---
+++ db/migrate/20161119120549_create_people.rb
@@ -0,0 +1,10 @@
+class CreatePeople < ActiveRecord::Migration[5.0]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :age
+
+      t.timestamps
+    end
+  end
+end
